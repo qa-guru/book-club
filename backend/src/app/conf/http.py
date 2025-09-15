b143fee398c03ce0837c26ee40c572ca106de8e3
John Makarov <johnmakarovqa@gmail.com>
2025-09-15 12:12:33 +0300

    fix csrf, cors allowed origins

--- backend/src/app/conf/http.py
+++ backend/src/app/conf/http.py
@@ -1,12 +1,2 @@
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:4173",
-    "http://localhost",
-    "https://bookclub.qa.guru/"
-]
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:4173",
-    "http://localhost",
-    "https://bookclub.qa.guru/"
-]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://localhost:4173", "http://localhost", "https://bookclub.qa.guru"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:5173", "http://localhost:4173", "http://localhost", "https://bookclub.qa.guru"]
