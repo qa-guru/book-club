f872ae15cc8de1b3144b0dd3e70c03bbc08fca9f
John Makarov <johnmakarovqa@gmail.com>
2025-09-18 20:47:57 +0300

    add new csrf, cors origins

--- backend/src/app/conf/http.py
+++ backend/src/app/conf/http.py
@@ -1,2 +1,16 @@
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://localhost:4173", "http://localhost", "https://bookclub.qa.guru"]
-CSRF_TRUSTED_ORIGINS = ["http://localhost:5173", "http://localhost:4173", "http://localhost", "https://bookclub.qa.guru"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    "http://localhost",
+    "https://bookclub.qa.guru",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    "http://localhost",
+    "https://bookclub.qa.guru",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1",
+]
