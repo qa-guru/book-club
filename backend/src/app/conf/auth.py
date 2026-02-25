b24d986bff1321f1d37e55b77406a4afc23461f3
John Makarov <johnmakarovqa@gmail.com>
2026-02-25 17:49:34 +0300

    change VITE_API_BASE_URL to http://localhost:8000

--- backend/src/app/conf/auth.py
+++ backend/src/app/conf/auth.py
@@ -12,8 +12,8 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(hours=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=24),
     "AUTH_HEADER_TYPES": ("Bearer",),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
