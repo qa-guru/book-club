b75730d116ac678cf81b824492728242396605ef
George <12208636+georg3k@users.noreply.github.com>
2026-03-08 04:11:45 +0300

    fix probes

--- backend/src/app/conf/http.py
+++ backend/src/app/conf/http.py
@@ -11,7 +11,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://bookclub.qa.guru:8000",
 ]
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bookclub.qa.guru"]
+ALLOWED_HOSTS = ["10.0.0.0/8", "localhost", "127.0.0.1", "bookclub.qa.guru"]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
