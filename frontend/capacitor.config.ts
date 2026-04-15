b273a4fe11de7e866411c1ab7b04e2e2caea9258
John Makarov <johnmakarovqa@gmail.com>
2026-04-15 13:02:25 +0300

    update capacitor.config.ts

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'guru.qa.bookclub',
   appName: 'book-club-ui',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true, 
+    },
+  },
 };
 
 export default config;
